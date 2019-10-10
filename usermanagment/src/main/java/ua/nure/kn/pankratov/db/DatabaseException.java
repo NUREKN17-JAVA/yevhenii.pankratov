60a405a8a04d63c980ca0a769025150d84114f1c
yevhenii pankratov <yevhenii.pankratov@nure.ua>
2019-10-10 11:23:49 +0300

    creating a findAll method and testing method

--- usermanagment/src/main/java/ua/nure/kn/pankratov/db/DatabaseException.java
+++ usermanagment/src/main/java/ua/nure/kn/pankratov/db/DatabaseException.java
@@ -1,4 +1,11 @@
 package main.java.ua.nure.kn.pankratov.db;
 
 public class DatabaseException extends Exception {
+    public DatabaseException(Exception e) {
+        super(e);
+    }
+
+    public DatabaseException(String string) {
+        super(string);
+    }
 }
